cd6517c0deb223baddd01b05407df6109dea7d46
indrisetyani987 <indrisetyani987@gmail.com>
2025-12-15 02:05:57 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -36,7 +36,7 @@ fun_bar() {
 }
 res1() {
     wget https://raw.githubusercontent.com/indrisetyani987/SCRIPTMULTIPLUS/main/menu/menu.zip
-    7z x -pMULTIPLUS menu.zip
+    unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
     rm -rf menu
